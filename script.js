49480a27d5523d8c69b023040988618e763762ef
BenjaminFrias <benjaminfrias2004@gmail.com>
2024-04-02 17:51:33 -0400

    basic js functions

---
+++ script.js
@@ -0,0 +1,37 @@
+var a = 2;
+var b = 5;
+var operator = "+";
+var result;
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return;
+    }
+}
+
+console.log(operate(a, b, operator))
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
